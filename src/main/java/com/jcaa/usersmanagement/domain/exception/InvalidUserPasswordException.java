aeec0c843083a87dcb59c5c0e89882aace35ec46
maverickd18 <maverickdiazserna@gmail.com>
2026-04-20 00:00:39 -0400

    fix/regla 10 y 18

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
@@ -1,17 +1,21 @@
 package com.jcaa.usersmanagement.domain.exception;
 
+
 public final class InvalidUserPasswordException extends DomainException {
+
+  private static final String PASSWORD_EMPTY_MESSAGE = "The user password must not be empty.";
+  private static final String PASSWORD_TOO_SHORT_TEMPLATE = "The user password must have at least %d characters.";
 
   private InvalidUserPasswordException(final String message) {
     super(message);
   }
 
   public static InvalidUserPasswordException becauseValueIsEmpty() {
-    return new InvalidUserPasswordException("The user password must not be empty.");
+    return new InvalidUserPasswordException(PASSWORD_EMPTY_MESSAGE);
   }
 
   public static InvalidUserPasswordException becauseLengthIsTooShort(final int minimumLength) {
     return new InvalidUserPasswordException(
-        String.format("The user password must have at least %d characters.", minimumLength));
+        String.format(PASSWORD_TOO_SHORT_TEMPLATE, minimumLength));
   }
 }
